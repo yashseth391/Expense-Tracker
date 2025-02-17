bee99a3c566afe84fb1d626d97fbbbc6a57bd87b
Yash Seth <yashseth391@gmail.com>
2025-02-18 02:04:15 +0530

    added welcome screen ui and login

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,65 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import BackButton from "@/components/BackButton";
+
+const Login = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <BackButton />
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Hey,
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Welcome Back!
+          </Typo>
+        </View>
+        {/* form */}
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLighter}>
+            Login
+          </Typo>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 5,
+    alignItems: "center",
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
---
+++ components/input.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const input = () => {
+  return (
+    <View>
+      <Text>input</Text>
+    </View>
+  );
+};
+
+export default input;
+
+const styles = StyleSheet.create({});
